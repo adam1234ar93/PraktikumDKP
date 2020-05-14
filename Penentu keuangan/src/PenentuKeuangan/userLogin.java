a84d5d46af0ceb413b621335021bce138efad6a8
adam1234ar93 <39836535+adam1234ar93@users.noreply.github.com>
2020-05-15 05:29:32 +0700

    Add files via upload

---
+++ Penentu keuangan/src/PenentuKeuangan/userLogin.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PenentuKeuangan;
+
+/**
+ *
+ * @author Adam Abdur Rozaq
+ */
+public class userLogin {
+    String penyemangat;
+    String oke;
+    
+    
+    public userLogin() /* deklarasi constructor userlogin */
+    {
+        this.penyemangat=" kamu yakin ?";
+        this.oke="kamu hebat";
+        
+    }
+}
